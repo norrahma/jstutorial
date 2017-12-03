0e4f2d8ca3ab492b5964d344c17f3a1a77260fa8
ymeddah <my.meddah@gmail.com>
2017-12-03 18:13:14 +0100

    listen input event on inputField

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
-var monObjet = $('h1#tutorialName');
-if(monObjet) {
-    monObjet.css("color", "#ff0000");
-    console.log(monObjet);
-}
+var API = "https://api.github.com/users/";
+var $champDeRecherche = $('#champDeRecherche');
+$champDeRecherche.on('input', function(){
+    var user = this.value;
+    if (user) {
+        console.log(API+user+"/repos");
+
+    }
+});
